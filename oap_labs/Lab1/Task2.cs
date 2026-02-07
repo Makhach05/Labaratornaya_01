ec04d025fc8207ebd988243da000f60d5620b71a
Makh4ch <abdulsaidovmahach43@gmail.com>
2026-02-07 19:57:00 +0300

    Lab1: решение 3 задач

---
+++ oap_labs/Lab1/Task2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace oap_labs.Lab1
+{
+    public static class Task2
+    {
+        public static void Solve()
+        {
+            Console.WriteLine("=== Прямоугольный треугольник ===");
+            
+            Console.Write("Введите длину первого катета: ");
+            double katet1 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Введите длину второго катета: ");
+            double katet2 = Convert.ToDouble(Console.ReadLine());
+            
+            double hypotenuse = Math.Sqrt(katet1 * katet1 + katet2 * katet2);
+            double area = (katet1 * katet2) / 2;
+            double perimeter = katet1 + katet2 + hypotenuse;
+            
+            Console.WriteLine($"Гипотенуза: {hypotenuse:F2}");
+            Console.WriteLine($"Площадь: {area:F2}");
+            Console.WriteLine($"Периметр: {perimeter:F2}");
+            Console.WriteLine();
+        }
+    }
+}
--- oap_labs/Program.cs
+++ oap_labs/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using oap_labs.Lab1;
 
-Console.WriteLine("Hello, World!");
+namespace oap_labs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа №1");
+            Console.WriteLine("======================\n");
+            
+            // Запуск решения всех задач
+            Task1.Solve();
+            Task2.Solve();
+            Task3.Solve();
+            
+            // Ожидание нажатия Enter
+            Console.Write("Нажмите Enter для выхода...");
+            Console.ReadLine();
+        }
+    }
+}
